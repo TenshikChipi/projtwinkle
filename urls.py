e10b2d6739fd85d0c5c7643e75147866afceb4ec
TenshikChipi <dashatuzovs@gmail.com>
2024-11-22 16:03:25 +0300

    urls.py

--- urls.py
+++ urls.py
@@ -2,5 +2,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.index), 
+    path('',views.index),
+    path('',views.autumn),
+    path('',views.colottype), 
+    path('',views.log),
+    path('',views.razdeli),
+    path('',views.spring),
+    path('',views.summer),
+    path('',views.winter),
 ]
