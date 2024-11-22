085ba021eb814e4de974e8d1eff4f0b263fae498
TenshikChipi <dashatuzovs@gmail.com>
2024-11-22 16:04:20 +0300

    views.py

--- views.py
+++ views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 def index(request):
     return render(request, "main/index.html")
+def autumn(request):
+    return render(request, "main/autumn.html")
+def colottype(request):
+    return render(request, "templates/colottype.html")
+def log(request):
+    return render(request, "templates/log.html")
+def razdeli(request):
+    return render(request, "templates/razdeli.html")
+def spring(request):
+    return render(request, "templates/spring.html")
+def summer(request):
+    return render(request, "templates/summer.html")
+def winter(request):
+    return render(request, "templates/winter.html")    
